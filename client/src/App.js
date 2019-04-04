c5f6d8f2845a92b7727cca75e3dc64f597b95a78
Michael Kerl <michaelkerl16@gmail.com>
2019-04-03 23:19:09 -0600

    add <App /> tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './App.css';
-
-import Landing from './components/container/Landing';
-import Page from './routes/Page';
 
 import storeConfig from './storeConfig';
 
-const store = storeConfig();
+import Routes from './components/routes';
 
-const App = () => (
-	<Provider store={store} data-testid="appComponent">
-		<Router>
-			<Switch>
-				<Route path="/" exact component={Landing} />
-				<Route component={Page} />
-			</Switch>
-		</Router>
-	</Provider>
-);
+export default function App() {
+	const store = storeConfig();
 
-export default App;
+	return (
+		<Provider store={store} data-testid="App">
+			<Routes />
+		</Provider>
+	);
+}
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,44 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
 import { findByTestId } from './testUtils';
-import storeConfig from './storeConfig';
 
-const setUp = (initialState = {}) => {
-	const store = storeConfig(initialState);
-	const component = shallow(<App store={store} />);
-	return component;
+import Component from './App';
+
+const setUp = () => {
+	const enzymeWrapper = shallow(<Component />);
+	return {
+		enzymeWrapper,
+	};
 };
 
-describe('<App>', () => {
-	let component;
+/* ********************
+         APP
+******************** */
+describe('<App />', () => {
+	let wrapper;
 
 	beforeEach(() => {
-		const initialState = {
-			user: {
-				suggested_users: [
-					{
-						name: 'John',
-						screen_name: 'johnsmith',
-						profile_image_url_https:
-							'http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png',
-						verified: true,
-					},
-					{
-						name: 'Adam',
-						screen_name: 'adamwright',
-						profile_image_url_https:
-							'http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png',
-						verified: false,
-					},
-				],
-			},
-		};
-		component = setUp(initialState);
+		const { enzymeWrapper } = setUp();
+
+		wrapper = enzymeWrapper;
 	});
 
 	it('should render without errors', () => {
-		const wrapper = findByTestId(component, 'appComponent');
-		expect(wrapper.length).toBe(1);
+		expect(findByTestId(wrapper, 'App').length).toEqual(1);
 	});
 });
