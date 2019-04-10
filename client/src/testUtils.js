6a62e3e6b37fa93ff160c0ec9cc9fd8787e91e87
Michael Kerl <michaelkerl16@gmail.com>
2019-04-09 22:09:36 -0600

    update test utils

--- client/src/testUtils.js
+++ client/src/testUtils.js
@@ -1,4 +1,8 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import checkPropTypes from 'check-prop-types';
+
+export const mockStore = configureMockStore([thunk]);
 
 export const findByTestId = (component, id) => {
 	return component.find(`[data-testid='${id}']`);
