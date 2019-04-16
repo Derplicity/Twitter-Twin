ee9a4a51cf41e20c02092569e47c7d6e7371ace4
Michael Kerl <michaelkerl16@gmail.com>
2019-04-16 16:59:56 -0600

    refactor for tests

--- client/src/components/presentational/Action.js
+++ client/src/components/presentational/Action.js
@@ -1,45 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Action, Icon, Text } from '../styles';
 
-export default function({ type, isSmall, isActive, count, onAction }) {
+const propTypes = {
+  type: PropTypes.string,
+  isSmall: PropTypes.bool,
+  isActive: PropTypes.bool,
+  count: PropTypes.number, // should be changed to string
+  onActive: PropTypes.func,
+};
+
+const defaultProps = {
+  type: null,
+  isSmall: false,
+  isActive: false,
+  count: 0,
+  onActive: () => null,
+};
+
+export function ActionPresentator(props) {
+  const { type, isSmall, isActive, count, onAction } = props;
+
+  if (!type) return null;
+
+  const colors = {
+    bubble:
+      type === 'heart'
+        ? 'red_lighter'
+        : type === 'retweet'
+        ? 'green_lighter'
+        : 'blueGrey__lighter',
+    icon: isActive
+      ? type === 'heart'
+        ? 'red'
+        : type === 'retweet'
+        ? 'green'
+        : 'blue'
+      : 'grey',
+    hover: type === 'heart' ? 'red' : type === 'retweet' ? 'green' : 'blue',
+  };
+
   return (
-    <Action>
+    <Action data-testid="ActionPresentator">
       <Icon.Interactive
-        transitionto={
-          type === 'heart' ? 'red' : type === 'retweet' ? 'green' : 'blue'
-        }
+        transitionto={colors.hover}
         onClick={() => onAction(type)}
+        data-testid="interactiveWrapper"
       >
         <Action.Group>
           <Icon.Container>
             <Icon.Bubble
               small
-              transitionto={
-                type === 'heart'
-                  ? 'red_lighter'
-                  : type === 'retweet'
-                  ? 'green_lighter'
-                  : 'blueGrey__lighter'
-              }
+              transitionto={colors.bubble}
+              data-testid="iconBubble"
             />
-            <Icon.Wrapper small={isSmall}>
+            <Icon.Wrapper small={isSmall} data-testid="iconWrapper">
               <Icon
                 icon={['fas', type]}
-                color={
-                  isActive
-                    ? type === 'heart'
-                      ? 'red'
-                      : type === 'retweet'
-                      ? 'green'
-                      : 'blue'
-                    : 'grey'
-                }
+                color={colors.icon}
+                data-testid="icon"
               />
             </Icon.Wrapper>
           </Icon.Container>
           {count > 0 && (
-            <Text small color="grey" style={{ marginLeft: '8px' }}>
+            <Text
+              small
+              color="grey"
+              style={{ marginLeft: '8px' }}
+              data-testid="countText"
+            >
               {count}
             </Text>
           )}
@@ -48,3 +78,8 @@ export default function({ type, isSmall, isActive, count, onAction }) {
     </Action>
   );
 }
+
+ActionPresentator.propTypes = propTypes;
+ActionPresentator.defaultProps = defaultProps;
+
+export default ActionPresentator;
