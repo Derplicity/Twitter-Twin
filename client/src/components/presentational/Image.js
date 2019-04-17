cb3687839d9f4dabebcf7c34f2af3c5243d25979
Michael Kerl <michaelkerl16@gmail.com>
2019-04-16 18:57:56 -0600

    refactor: Set up for tests

--- client/src/components/presentational/Image.js
+++ client/src/components/presentational/Image.js
@@ -1,19 +1,51 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 import { Image } from '../styles';
 
-export default function({ to, src, alt, isCircle, isSmall, custom }) {
+const propTypes = {
+  to: PropTypes.string,
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  isCircle: PropTypes.bool,
+  isSmall: PropTypes.bool,
+  custom: PropTypes.objectOf(PropTypes.string.isRequired),
+};
+
+const defaultProps = {
+  to: null,
+  src: null,
+  alt: '',
+  isCircle: false,
+  isSmall: false,
+  custom: null,
+};
+
+export function ImagePresentator(props) {
+  const { to, src, alt, isCircle, isSmall, custom } = props;
+
+  if (!src) return null;
+
   const imageContents = (
     <Fragment>
-      <Image.Overlay url={src} />
-      <Image src={src} alt={alt} />
+      <Image.Overlay url={src} data-testid="imageOverlay" />
+      <Image src={src} alt={alt} data-testid="image" />
     </Fragment>
   );
 
   return (
-    <Image.Wrapper circle={isCircle} small={isSmall}>
+    <Image.Wrapper
+      circle={isCircle}
+      small={isSmall}
+      data-testid="ImagePresentator"
+    >
       {to ? (
-        <Image.InternalLink to={to} tabIndex="-1" style={custom}>
+        <Image.InternalLink
+          to={to}
+          tabIndex="-1"
+          style={custom}
+          data-testid="imageLink"
+        >
           {imageContents}
         </Image.InternalLink>
       ) : (
@@ -22,3 +54,8 @@ export default function({ to, src, alt, isCircle, isSmall, custom }) {
     </Image.Wrapper>
   );
 }
+
+ImagePresentator.propTypes = propTypes;
+ImagePresentator.defaultProps = defaultProps;
+
+export default ImagePresentator;
