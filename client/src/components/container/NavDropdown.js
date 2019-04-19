caaa114a26ceefae523aadc72ecce90ecd574272
Michael Kerl <michaelkerl16@gmail.com>
2019-04-19 12:48:46 -0600

    refactor: Set up for tests

--- client/src/components/container/NavDropdown.js
+++ client/src/components/container/NavDropdown.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import DropdownContent from '../presentational/DropdownContent';
+import DropdownContentPresentator from '../presentational/DropdownContent';
 
 import {
   NavDropdownWrapper,
@@ -12,30 +12,50 @@ import {
   NavUserName,
 } from '../styled-components';
 
-class NavDropdown extends React.Component {
-  state = {
-    isDropped: false,
-  };
+const propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    screen_name: PropTypes.string.isRequired,
+    profile_image_url_https: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+export class NavDropdownContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDropped: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
+  }
 
   componentWillUnmount() {
     document.removeEventListener('mousedown', this.handleClick, false);
   }
 
-  handleClick = e => {
+  handleClick(e) {
     if (this.node.contains(e.target)) {
-      return;
+      return null;
     }
     this.handleClickOutside();
-  };
+  }
 
-  handleClickOutside = () => {
+  handleClickOutside() {
     this.setState({
       isDropped: false,
     });
     document.removeEventListener('mousedown', this.handleClick, false);
-  };
+  }
 
-  handleDrop = () => {
+  handleDrop() {
     this.setState(
       {
         isDropped: !this.state.isDropped,
@@ -43,13 +63,16 @@ class NavDropdown extends React.Component {
       () => (document.getElementById('drop-menu').scrollTop = 0),
     );
     document.addEventListener('mousedown', this.handleClick, false);
-  };
+  }
 
   render() {
     const { user } = this.props;
     const { isDropped } = this.state;
+
+    if (!user) return null;
+
     return (
-      <NavDropdownWrapper>
+      <NavDropdownWrapper data-testid="NavDropdownContainer">
         <NavDropdownLink
           role="button"
           tabIndex="0"
@@ -57,26 +80,31 @@ class NavDropdown extends React.Component {
           onKeyDown={e =>
             e.keyCode === 13 && !e.shiftKey ? this.handleDrop() : null
           }
+          data-testid="dropLink"
         >
           <NavDropdownDrop tabIndex="-1">
-            <NavUserImg src={user.profile_image_url_https} alt="Profile" />
-            <NavUserName>{user.name}</NavUserName>{' '}
+            <NavUserImg
+              src={user.profile_image_url_https}
+              alt="Profile"
+              data-testid="profileImage"
+            />
+            <NavUserName data-testid="profileName">{user.name}</NavUserName>{' '}
             <FontAwesomeIcon icon={['fas', 'caret-down']} />
           </NavDropdownDrop>
         </NavDropdownLink>
-        <DropdownContent
+        <DropdownContentPresentator
           isDropped={isDropped}
           setNode={node => (this.node = node)}
           handleDrop={this.handleDrop}
           user={user}
+          data-testid="DropdownContentPresentator"
         />
       </NavDropdownWrapper>
     );
   }
 }
 
-NavDropdown.propTypes = {
-  user: PropTypes.object.isRequired,
-};
+NavDropdownContainer.propTypes = propTypes;
+NavDropdownContainer.defaultProps = defaultProps;
 
-export default NavDropdown;
+export default NavDropdownContainer;
