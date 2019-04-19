d0d9baa080c3807558298d94ee8b1b069b6cc219
Michael Kerl <michaelkerl16@gmail.com>
2019-04-19 14:29:01 -0600

    refactor: Set up for tests

--- client/src/components/presentational/ProfileSection.js
+++ client/src/components/presentational/ProfileSection.js
@@ -12,29 +12,71 @@ import {
   ProfileFollowNum,
 } from '../styled-components';
 
-const ProfileSection = ({ user, onClick }) => (
-  <ProfileSectionWrapper>
-    <ProfileLink to={`/${user.screen_name}`} onClick={onClick}>
-      <ProfileImg src={user.profile_image_url_https} alt={user.name} />
-      <ProfileName>{user.name}</ProfileName>
-      <ProfileUsername>@{user.screen_name}</ProfileUsername>
-    </ProfileLink>
-    <ProfileFollow>
-      <ProfileFollowLink to="/following" onClick={onClick}>
-        <ProfileFollowNum>{user.friends_count_formatted}</ProfileFollowNum>{' '}
-        Following
-      </ProfileFollowLink>
-      <ProfileFollowLink to="/followers" onClick={onClick}>
-        <ProfileFollowNum>{user.followers_count_formatted}</ProfileFollowNum>{' '}
-        Followers
-      </ProfileFollowLink>
-    </ProfileFollow>
-  </ProfileSectionWrapper>
-);
-
-ProfileSection.propTypes = {
-  user: PropTypes.object.isRequired,
-  onClick: PropTypes.func.isRequired,
+const propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    screen_name: PropTypes.string.isRequired,
+    profile_image_url_https: PropTypes.string.isRequired,
+    friends_count_formatted: PropTypes.string.isRequired,
+    followers_count_formatted: PropTypes.string.isRequired,
+  }),
+  onClick: PropTypes.func,
 };
 
-export default ProfileSection;
+const defaultProps = {
+  user: null,
+  onClick: () => null,
+};
+
+export function ProfileSectionPresentator(props) {
+  const { user, onClick } = props;
+
+  if (!user) return null;
+
+  return (
+    <ProfileSectionWrapper data-testid="ProfileSectionPresentator">
+      <ProfileLink
+        to={`/${user.screen_name}`}
+        onClick={onClick}
+        data-testid="profileLink"
+      >
+        <ProfileImg
+          src={user.profile_image_url_https}
+          alt={user.name}
+          data-testid="profileImage"
+        />
+        <ProfileName data-testid="profileName">{user.name}</ProfileName>
+        <ProfileUsername data-testid="profileUsername">
+          @{user.screen_name}
+        </ProfileUsername>
+      </ProfileLink>
+      <ProfileFollow>
+        <ProfileFollowLink
+          to="/following"
+          onClick={onClick}
+          data-testid="followLink"
+        >
+          <ProfileFollowNum data-testid="friendCount">
+            {user.friends_count_formatted}
+          </ProfileFollowNum>{' '}
+          Following
+        </ProfileFollowLink>
+        <ProfileFollowLink
+          to="/followers"
+          onClick={onClick}
+          data-testid="followLink"
+        >
+          <ProfileFollowNum data-testid="followCount">
+            {user.followers_count_formatted}
+          </ProfileFollowNum>{' '}
+          Followers
+        </ProfileFollowLink>
+      </ProfileFollow>
+    </ProfileSectionWrapper>
+  );
+}
+
+ProfileSectionPresentator.propTypes = propTypes;
+ProfileSectionPresentator.defaultProps = defaultProps;
+
+export default ProfileSectionPresentator;
